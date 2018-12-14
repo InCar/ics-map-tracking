7e9cfb9ccc4d3a452a75ea75c026cf0a2ed303e7
wellingtonjone <509750512@qq.com>
2018-12-14 16:57:41 +0800

    修改文件

--- src/main/resources/static/js/yb.js
+++ src/main/resources/static/js/yb.js
@@ -1,3 +1,14 @@
-function test() {
-    alert(document.getElementById("test").getAttribute("value"))
+ // 构造函数
+function Incar(dom) {
+    this._dom = dom;
+    console.log(this._dom)
+    this._dom.innerText = "经纬度"
+}
+
+// init 方法
+var maptrack = {}
+maptrack.init = function (dom) {
+    let track = new Incar(dom);
+    console.log(track)
+    return track;
 }
