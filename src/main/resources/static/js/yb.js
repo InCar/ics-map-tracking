d7dc76002ae3a64af8495b9097a78dbd96e0e02e
wellingtonjone <509750512@qq.com>
2018-12-14 17:05:34 +0800

    固定只能写id

--- src/main/resources/static/js/yb.js
+++ src/main/resources/static/js/yb.js
@@ -1,14 +1,13 @@
  // 构造函数
 function Incar(dom) {
     this._dom = dom;
-    console.log(this._dom)
     this._dom.innerText = "经纬度"
 }
 
 // init 方法
 var maptrack = {}
 maptrack.init = function (dom) {
-    let track = new Incar(dom);
-    console.log(track)
+    let initDom = document.getElementById(dom);
+    let track = new Incar(initDom);
     return track;
 }
